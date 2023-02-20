97b11802f9341136ba990c4521798afd9fcaa1f1
GianK128 <giankeberlein@gmail.com>
2023-02-19 23:07:40 -0300

    Add loading state to trendingGifs hook

--- src/hooks/useTrendingGifs.hooks.ts
+++ src/hooks/useTrendingGifs.hooks.ts
@@ -4,10 +4,14 @@ import { getTrendingGifs } from '../services/gifs.services';
 
 export default function useTrendingGifs(params: GifSearchParams) {
     const [gifs, setGifs] = useState<GifData[]>([]);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
-        getTrendingGifs(params).then((data: GifResponse) => setGifs(data.data));
+        getTrendingGifs(params).then((data: GifResponse) => {
+            setGifs(data.data);
+            setLoaded(true);
+        });
     }, []);
 
-    return gifs;
+    return { trendingGifs: gifs, loadedGifs: loaded };
 }
